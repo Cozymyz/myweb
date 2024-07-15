287a7fb3c4c347cb8cf94dfa8648c6030443fed7
Mei Yizhi <v23e3024@oita-u.ac.jp>
2024-07-15 14:57:44 +0000

    ユーザ認証機能の完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  resources :users
+  resources :sessions
   root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
